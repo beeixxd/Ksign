fc1755ac54be00908868916d5fe284a1f4a2540a
Nyasami <80683081+Nyasami@users.noreply.github.com>
2026-05-25 04:36:35 +0700

    chore: fix credits

--- Ksign/Views/Settings/About/AboutView.swift
+++ Ksign/Views/Settings/About/AboutView.swift
@@ -14,7 +14,7 @@ struct AboutView: View {
 	@State private var _credits: [CreditsModel] = [
 		.init(name: "C", desc: "Developer", github: "claration"),
 		.init(name: "Asami", desc: "Developer", github: "Nyasami"),
-		.init(name: "Lakhan Lothiyi", desc: "AltStore Repositories", github: "llsc"),
+		.init(name: "Lakhan Lothiyi", desc: "AltStore Repositories", github: "llsc12"),
 	]
 	// MARK: Body
 	var body: some View {
